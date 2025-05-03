519ddf372df4fb0b944e2beb436b91b62e1db214
Nathaniel Ketema <nathanielketema@gmail.com>
2025-05-03 09:08:55 -0500

    feat: make telescope preview .dotfiles

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -18,7 +18,9 @@ return {
 
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader><leader>", ":Telescope<CR>", {})
-            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>ff", function()
+                require("telescope.builtin").find_files({ hidden = true })
+            end, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
             vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
             vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
