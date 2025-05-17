1b85f2c48f66b69c5613c04c97bb832280ca8d11
Nathaniel Ketema <nathanielketema@gmail.com>
2025-05-16 22:57:08 -0500

    feat: add zls lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -46,6 +46,9 @@ return {
                     },
                 },
             })
+            lspconfig.zls.setup({
+                capabilities = capabilities,
+            })
             lspconfig.html.setup({
                 capabilities = capabilities,
             })
