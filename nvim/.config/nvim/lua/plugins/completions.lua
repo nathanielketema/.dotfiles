1bd113449491fc21362c19ea76cb7cf25945fabe
Nathaniel Ketema <nathanielketema@gmail.com>
2025-09-10 17:04:09 -0500

    feat: change keymap for completion

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -46,7 +46,7 @@ return {
                     ["<C-f>"] = cmp.mapping.scroll_docs(2),
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
                 }),
                 sources = cmp.config.sources({
                     { name = "nvim_lsp", priority = 1000 },
