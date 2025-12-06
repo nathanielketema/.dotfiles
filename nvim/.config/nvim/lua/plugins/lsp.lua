082ef298183490ab4be49be3b51ed9aad0e0c03d
Nathaniel Ketema <nathanielketema@gmail.com>
2025-12-06 16:39:12 -0600

    feat: update to new lsp api

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,142 +1,113 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        lazy = false,
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = false,
+			handlers = {},
+		},
+	},
+	{
+		"nvim-java/nvim-java",
+		lazy = false,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
 
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        lazy = false,
-        opts = {
-            auto_install = false,
-            handlers = {},
-        },
-    },
-    {
-        "nvim-java/nvim-java",
-        lazy = false,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
+		config = function()
+			require("java").setup()
 
-        config = function()
-            require("java").setup()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local servers = {
+				"ts_ls",
+				"ruff",
+				"html",
+				"lua_ls",
+				"clangd",
+				"csharp_ls",
+				"cssls",
+				"jsonls",
+				"marksman",
+				"yamlls",
+			}
 
-            local lspconfig = require("lspconfig")
-            lspconfig.jdtls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.ts_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.ruff.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.html.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.clangd.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.csharp_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.cssls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.jsonls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.marksman.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.yamlls.setup({
-                capabilities = capabilities,
-            })
-            --[[
-            lspconfig.zls.setup({
-                capabilities = capabilities,
-                settings = {
-                    zls = {
-                        zig_exe_path = "/Users/nathaniel/.zig//zig_source",
-                    },
-                },
-            })
-            ]]
+			for _, server in ipairs(servers) do
+				vim.lsp.config(server, {
+					capabilities = capabilities,
+				})
+			end
 
-            vim.diagnostic.config({
-                virtual_text = {
-                    spacing = 2,
-                },
-                signs = false,
-                underline = true,
-                update_in_insert = false,
-                severity_sort = true,
-            })
+			vim.lsp.config("jdtls", {
+				capabilities = capabilities,
+			})
 
-            vim.keymap.set("n", "gd", function()
-                vim.lsp.buf.definition()
-            end, {})
-            vim.keymap.set("n", "K", function()
-                vim.lsp.buf.hover()
-            end, {})
-            vim.keymap.set("n", "<leader>ws", function()
-                vim.lsp.buf.workspace_symbol()
-            end, {})
-            vim.keymap.set("n", "<leader>vd", function()
-                vim.diagnostic.open_float()
-            end, {})
-            vim.keymap.set("n", "[d", function()
-                vim.diagnostic.goto_next()
-            end, {})
-            vim.keymap.set("n", "]d", function()
-                vim.diagnostic.goto_prev()
-            end, {})
-            vim.keymap.set("n", "<leader>ca", function()
-                vim.lsp.buf.code_action()
-            end, {})
-            vim.keymap.set("n", "<leader>rr", function()
-                vim.lsp.buf.references()
-            end, {})
-            vim.keymap.set("n", "<leader>rn", function()
-                vim.lsp.buf.rename()
-            end, {})
-            vim.keymap.set("n", "<leader>vc", function()
-                vim.diagnostic.config({
-                    virtual_text = false,
-                    signs = false,
-                    underline = false,
-                })
-            end, {})
-            vim.keymap.set("n", "<leader>vs", function()
-                vim.diagnostic.config({
-                    virtual_text = {
-                        spacing = 2,
-                    },
-                    signs = false,
-                    underline = true,
-                    update_in_insert = false,
-                    severity_sort = true,
-                    float = {
-                        border = "rounded",
-                        max_width = 80,
-                        max_height = 20,
-                    },
-                })
-            end, {})
+			for _, server in ipairs(servers) do
+				vim.lsp.enable(server)
+			end
 
-            vim.keymap.set("i", "<C-h>", function()
-                vim.lsp.buf.signature_help()
-            end, {})
-        end,
-    },
+			vim.lsp.enable("jdtls")
+
+			vim.diagnostic.config({
+				virtual_text = {
+					spacing = 2,
+				},
+				signs = false,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+			})
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("CustomLspAttach", { clear = true }),
+				callback = function(args)
+					local buf = args.buf
+
+					local opts = { buffer = buf }
+
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "<leader>ws", vim.lsp.buf.workspace_symbol, opts)
+					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "<leader>rr", vim.lsp.buf.references, opts)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+				end,
+			})
+
+			vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, {})
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_next, {})
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, {})
+
+			vim.keymap.set("n", "<leader>vc", function()
+				vim.diagnostic.config({
+					virtual_text = false,
+					signs = false,
+					underline = false,
+				})
+			end, {})
+			vim.keymap.set("n", "<leader>vs", function()
+				vim.diagnostic.config({
+					virtual_text = { spacing = 2 },
+					signs = false,
+					underline = true,
+					update_in_insert = false,
+					severity_sort = true,
+					float = {
+						border = "rounded",
+						max_width = 80,
+						max_height = 20,
+					},
+				})
+			end, {})
+		end,
+	},
 }
