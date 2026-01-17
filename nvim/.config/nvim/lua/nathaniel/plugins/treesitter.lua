418d172405351b0e45b5b804bad7da70c44ab92f
Nathaniel Ketema <nathanielketema@gmail.com>
2026-01-16 18:55:12 -0600

    feat: treesitter on steroids

--- nvim/.config/nvim/lua/nathaniel/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/nathaniel/plugins/treesitter.lua
@@ -1,25 +1,66 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        event = { "BufReadPost", "BufNewFile" },
-        opts = {
-            ensure_installed = {
-                "zig", "python", "c", "cpp", "lua", "markdown", "markdown_inline", "html", "css",
-            },
-
-            sync_install = false,
-            auto_install = true,
-
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = { "markdown" },
-            },
-
-            indent = { enable = true },
-        },
-        config = function(_, opts)
-            require("nvim-treesitter.configs").setup(opts)
-        end,
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-textobjects",
     },
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+        ensure_installed = {
+            "zig", "python", "c", "cpp", "lua", "markdown", "markdown_inline", "html", "css",
+        },
+
+        sync_install = false,
+        auto_install = true,
+
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = { "markdown" },
+        },
+
+        indent = { enable = true },
+
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<C-h>",
+                node_incremental = "<C-h>",
+                scope_incremental = false,
+                node_decremental = "<bs>",
+            },
+        },
+
+        textobjects = {
+            select = {
+                enable = true,
+                lookahead = true,
+                keymaps = {
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["ac"] = "@class.outer",
+                    ["ic"] = "@class.inner",
+                    ["ai"] = "@conditional.outer",
+                    ["ii"] = "@conditional.inner",
+                    ["al"] = "@loop.outer",
+                    ["il"] = "@loop.inner",
+                },
+            },
+            move = {
+                enable = true,
+                set_jumps = true,
+                goto_next_start = {
+                    ["]f"] = "@function.outer",
+                    ["]c"] = "@class.outer",
+                },
+                goto_previous_start = {
+                    ["[f"] = "@function.outer",
+                    ["[c"] = "@class.outer",
+                },
+            },
+        },
+    },
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+    end,
 }
