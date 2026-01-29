81b769a6a2adf8b6d8ea27b49eaf0962de7c5324
Nathaniel Ketema <nathanielketema@gmail.com>
2026-01-29 08:16:49 -0600

    feat: add catpuccin

--- nvim/.config/nvim/lua/nathaniel/plugins/color.lua
+++ nvim/.config/nvim/lua/nathaniel/plugins/color.lua
@@ -1,35 +1,42 @@
 return {
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {
-            styles = {
-                transparent = "tokyonight_transparent",
-                sidebars = "tokyonight_transparent",
-            },
-        },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			styles = {
+				transparent = "tokyonight_transparent",
+				sidebars = "tokyonight_transparent",
+			},
+		},
 
-        config = function(_, opts)
-            require("tokyonight").setup(opts)
-            vim.opt.background = "dark"
-            vim.cmd.colorscheme("tokyonight-night")
-        end,
-    },
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        lazy = false,
-        priority = 1000,
-        opts = {
-            variant = "main",
-            dim_inactive_windows = false,
-            extend_background_behind_borders = true,
-        },
-        config = function(_, opts)
-            require("rose-pine").setup(opts)
-            vim.opt.background = "dark"
-            --vim.cmd.colorscheme("rose-pine")
-        end,
-    },
+		config = function()
+			vim.opt.background = "dark"
+			vim.cmd.colorscheme("tokyonight-night")
+		end,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			variant = "main",
+			dim_inactive_windows = false,
+			extend_background_behind_borders = true,
+		},
+		config = function()
+			vim.opt.background = "dark"
+			--vim.cmd.colorscheme("rose-pine")
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {},
+		config = function()
+			vim.opt.background = "dark"
+		end,
+	},
 }
