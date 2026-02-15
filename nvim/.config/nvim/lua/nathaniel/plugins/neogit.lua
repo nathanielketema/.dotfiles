86293ec60791f3276b4c47b452bf5636da3b0341
Nathaniel Ketema <nathanielketema@gmail.com>
2026-02-15 12:00:52 -0600

    feat: edit neogit keymap

--- nvim/.config/nvim/lua/nathaniel/plugins/neogit.lua
+++ nvim/.config/nvim/lua/nathaniel/plugins/neogit.lua
@@ -7,12 +7,24 @@ return {
 	},
 	cmd = "Neogit",
 	keys = {
-		{ "<leader>g", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+		{
+			"<leader>g",
+			function()
+				require("neogit").open({ kind = "tab" })
+			end,
+			desc = "Show Neogit UI",
+		},
+		{
+			"<leader>m",
+			function()
+				require("neogit").open({ kind = "vsplit" })
+			end,
+			desc = "Show Neogit UI",
+		},
 	},
 	opts = {
 		disable_hint = true,
 		disable_context_highlighting = true,
-		kind = "vsplit",
 		disable_line_numbers = false,
 		disable_relative_line_numbers = false,
 		signs = {
@@ -20,50 +32,6 @@ return {
 			hunk = { "", "" },
 			item = { "▶︎", "▼" },
 			section = { "▶︎", "▼" },
-
-		},
-		commit_editor = {
-			kind = "auto",
-			show_staged_diff = true,
-			-- Accepted values:
-			-- "split" to show the staged diff below the commit editor
-			-- "vsplit" to show it to the right
-			-- "split_above" Like :top split
-			-- "vsplit_left" like :vsplit, but open to the left
-			-- "auto" "vsplit" if window would have 80 cols, otherwise "split"
-			staged_diff_split_kind = "split",
-			spell_check = true,
-		},
-		commit_select_view = {
-			kind = "auto",
-		},
-		commit_view = {
-			kind = "auto",
-			verify_commit = vim.fn.executable("gpg") == 1,
-		},
-		log_view = {
-			kind = "tab",
-		},
-		rebase_editor = {
-			kind = "auto",
-		},
-		reflog_view = {
-			kind = "tab",
-		},
-		merge_editor = {
-			kind = "auto",
-		},
-		preview_buffer = {
-			kind = "floating_console",
-		},
-		popup = {
-			kind = "split",
-		},
-		stash = {
-			kind = "tab",
-		},
-		refs_view = {
-			kind = "tab",
 		},
 	},
 }
