3f72a330e0c6da9f64b4836cf909ca7d4032af8c
Nathaniel Ketema <nathanielketema@gmail.com>
2026-03-21 10:43:26 -0500

    feat: orgmode

---
+++ nvim/.config/nvim/lua/nathaniel/plugins/orgmode.lua
@@ -0,0 +1,17 @@
+return {
+	"nvim-orgmode/orgmode",
+	event = "VeryLazy",
+	config = function()
+		-- Setup orgmode
+		require("orgmode").setup({
+			org_agenda_files = "~/personal/notes/orgfiles/**/*",
+			org_default_notes_file = "~/personal/notes/orgfiles/refile.org",
+			org_agenda_skip_deadline_prewarning_if_scheduled = true,
+			org_agenda_skip_scheduled_if_done = true,
+			org_agenda_skip_deadline_if_done = true,
+			org_deadline_warning_days = 0,
+		})
+		-- Experimental LSP support
+		vim.lsp.enable("org")
+	end,
+}
