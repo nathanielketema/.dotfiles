457fbbfa2d5e24117b22e41c6558aeca4150ca4e
Nathaniel Ketema <nathanielketema@gmail.com>
2026-04-20 17:02:26 -0500

    remove catpuccin

--- nvim/.config/nvim/lua/nathaniel/plugins/color.lua
+++ nvim/.config/nvim/lua/nathaniel/plugins/color.lua
@@ -30,13 +30,4 @@ return {
 			--vim.cmd.colorscheme("rose-pine")
 		end,
 	},
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		opts = {},
-		config = function()
-			vim.opt.background = "dark"
-		end,
-	},
 }
