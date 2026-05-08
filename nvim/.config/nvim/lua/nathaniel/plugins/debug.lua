238b382a1f040a5bbf1bb2c337620965462c2edd
Nathaniel Ketema <nathanielketema@gmail.com>
2026-05-08 10:13:07 -0500

    make debugging zig a lot easier

--- nvim/.config/nvim/lua/nathaniel/plugins/debug.lua
+++ nvim/.config/nvim/lua/nathaniel/plugins/debug.lua
@@ -1,86 +1,96 @@
 return {
-    {
-        "mfussenegger/nvim-dap",
-        dependencies = {
-            "rcarriga/nvim-dap-ui",
-            "nvim-neotest/nvim-nio",
-            "mfussenegger/nvim-dap-python",
-            "theHamsta/nvim-dap-virtual-text",
-        },
-        ft = { "python", "zig" },
-        opts = {
-            python_path = "python3",
-            codelldb_path = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
-        },
-        config = function(_, opts)
-            local dap = require("dap")
-            local dapui = require("dapui")
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			"nvim-neotest/nvim-nio",
+			"mfussenegger/nvim-dap-python",
+			"theHamsta/nvim-dap-virtual-text",
+		},
+		ft = { "python", "zig" },
+		opts = {
+			python_path = "python3",
+			codelldb_path = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+		},
+		config = function(_, opts)
+			local dap = require("dap")
+			local dapui = require("dapui")
 
-            require("nvim-dap-virtual-text").setup({})
-            dapui.setup()
+			require("nvim-dap-virtual-text").setup({})
+			dapui.setup()
 
-            --- 1. Python Setup ---
-            require("dap-python").setup(opts.python_path)
+			--- 1. Python Setup ---
+			require("dap-python").setup(opts.python_path)
 
-            --- 2. Zig / LLDB Setup ---
-            dap.adapters.codelldb = {
-                type = "server",
-                port = "${port}",
-                executable = {
-                    command = opts.codelldb_path,
-                    args = { "--port", "${port}" },
-                },
-            }
+			--- 2. Zig / LLDB Setup ---
+			dap.adapters.codelldb = {
+				type = "server",
+				port = "${port}",
+				executable = {
+					command = opts.codelldb_path,
+					args = { "--port", "${port}" },
+				},
+			}
 
-            dap.configurations.zig = {
-                {
-                    name = "Launch",
-                    type = "codelldb",
-                    request = "launch",
-                    program = function()
-                        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/zig-out/bin/", "file")
-                    end,
-                    cwd = "${workspaceFolder}",
-                    stopOnEntry = false,
-                    args = {},
-                },
-                {
-                    name = "Launch (with args)",
-                    type = "codelldb",
-                    request = "launch",
-                    program = function()
-                        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/zig-out/bin/", "file")
-                    end,
-                    cwd = "${workspaceFolder}",
-                    stopOnEntry = false,
-                    args = function()
-                        local args_string = vim.fn.input("Arguments: ")
-                        return vim.split(args_string, " +")
-                    end,
-                },
-            }
+			local zig_lldb_init = {
+				"command script import /Users/nathaniel/zig/tools/lldb_pretty_printers.py",
+				"type category enable zig.lang",
+				"type category enable zig.std",
+				"type category enable zig.stage2",
+				"type category enable zig",
+			}
 
-            --- 3. UI Listeners ---
-            -- Automatically open UI when debugging starts
-            dap.listeners.after.event_initialized["dapui_config"] = function()
-                dapui.open()
-            end
-            dap.listeners.before.event_terminated["dapui_config"] = function()
-                dapui.close()
-            end
-            dap.listeners.before.event_exited["dapui_config"] = function()
-                dapui.close()
-            end
+			dap.configurations.zig = {
+				{
+					name = "Launch",
+					type = "codelldb",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/zig-out/bin/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+					stopOnEntry = false,
+					args = {},
+					initCommands = zig_lldb_init,
+				},
+				{
+					name = "Launch (with args)",
+					type = "codelldb",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/zig-out/bin/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+					stopOnEntry = false,
+					args = function()
+						local args_string = vim.fn.input("Arguments: ")
+						return vim.split(args_string, " +")
+					end,
+					initCommands = zig_lldb_init,
+				},
+			}
 
-            --- 4. Keymaps ---
-            vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
-            vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "DAP Continue" })
-            vim.keymap.set("n", "<leader>ds", dap.step_into, { desc = "DAP Step Into" })
-            vim.keymap.set("n", "<leader>dn", dap.step_over, { desc = "DAP Step Over" })
-            vim.keymap.set("n", "<leader>do", dap.step_out, { desc = "DAP Step Out" })
-            vim.keymap.set("n", "<leader>dr", dap.restart, { desc = "DAP Restart" })
-            vim.keymap.set("n", "<leader>dt", dap.terminate, { desc = "DAP Terminate" })
-            vim.keymap.set("n", "<leader>dq", dap.close, { desc = "DAP Close Session" })
-        end,
-    },
+			--- 3. UI Listeners ---
+			-- Automatically open UI when debugging starts
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+
+			--- 4. Keymaps ---
+			vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+			vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "DAP Continue" })
+			vim.keymap.set("n", "<leader>ds", dap.step_into, { desc = "DAP Step Into" })
+			vim.keymap.set("n", "<leader>dn", dap.step_over, { desc = "DAP Step Over" })
+			vim.keymap.set("n", "<leader>do", dap.step_out, { desc = "DAP Step Out" })
+			vim.keymap.set("n", "<leader>dr", dap.restart, { desc = "DAP Restart" })
+			vim.keymap.set("n", "<leader>dt", dap.terminate, { desc = "DAP Terminate" })
+			vim.keymap.set("n", "<leader>dq", dap.close, { desc = "DAP Close Session" })
+		end,
+	},
 }
