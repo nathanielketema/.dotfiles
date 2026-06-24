e3a19af492ec73408de7488e2f3d68f8d193abe4
Nathaniel Ketema <nathanielketema@gmail.com>
2026-06-24 10:11:47 -0500

    fish changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,12 +10,12 @@ set --global --export FZF_CTRL_T_COMMAND "fd --type f"
 # Remove greeting
 set --global fish_greeting
 
-fish_add_path $HOME/personal/scripts
 fish_add_path $HOME/.zvm/self
 fish_add_path $HOME/.zvm/bin
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/go/bin/
+fish_add_path $HOME/personal/scripts/bin/
 fish_add_path $HOME/.outfieldr/zig-out/bin # zldr
 fish_add_path $HOME/git-clone/zignal/zig-out/bin
 fish_add_path $HOME/git-clone/zig/build/stage3/bin
@@ -26,10 +26,6 @@ fish_add_path /opt/homebrew/opt/curl/bin
 fzf --fish | source
 fish_user_key_bindings
 
-abbr pomo pomo.zig
-abbr config config.zig
-abbr mtp mtp.zig
-abbr ctp ctp.zig
 abbr ell "eza --long --tree --level 2 --icons --git --all"
 abbr els "eza --tree --level 2 --icons --git"
 abbr gitllog "git log --graph --oneline --decorate --all"
