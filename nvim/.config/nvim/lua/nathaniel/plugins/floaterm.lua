20cb08c1df1233da1a3d5918a1bcd8e7b121f3d9
Nathaniel Ketema <nathanielketema@gmail.com>
2026-06-25 12:04:38 -0500

    remove floaterm

--- nvim/.config/nvim/lua/nathaniel/plugins/floaterm.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "voldikss/vim-floaterm",
-
-    config = function()
-        vim.keymap.set("n", "<leader>y", ":FloatermNew --width=0.7 --height=0.8 yazi<CR>", { desc = "Open Yazi" })
-    end,
-}
